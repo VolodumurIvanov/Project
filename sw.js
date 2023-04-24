362180dac0aa54a42833942428609ced58e04bfc
Volodumur Ivanov <vokacol34@gmail.com>
2023-04-24 17:58:26 +0200

    agBblhkkn

--- sw.js
+++ sw.js
@@ -1,34 +1,27 @@
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
-    event.waitUntil(caches.open("static")
-        .then(function (cache) {
+    event.waitUntil(
+        caches.open("static")
+        .then(function(caches) {
             console.log("precaching");
-            cache.add('/index.html');
-            cache.add('/css/style.css');
-            cache.add('/css/header.css');
-            cache.add('/css/middle.css');
-            cache.add('/css/footer.css');
-            cache.add('/css/responsive.css');
-            cache.add('/images/png/logo.png');
-            cache.add('/');
+            caches.add('/index.html');
+            caches.add('/');
         })
     )
-});
-
-self.addEventListener('activate', function (event) {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-    return self.clients.claim();
-});
-self.addEventListener('fetch', function (event) {
-    console.log('[Service Worker] Fetching something ...', event);
+  });
+  self.addEventListener('activate', function(event) {
+ console.log('[Service Worker] Activating Service Worker ...', event);
+ return self.clients.claim();
+ });
+   self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
-            .then(function (response) {
-                if (response)
-                    return response;
+        .then(function(response) {
+            if (response)
+                return response;
                 else
-                    return fetch(event.request);
+                    return fetch(event.response);
             }
         )
-    )
-});
+    );
+  });
